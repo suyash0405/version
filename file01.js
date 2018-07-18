2c1f1485fd57320dd863650b7fb1dcd47ecf1e96
Suyash <suyash0405@gmail.com>
2018-07-18 15:41:32 -0700

    chaudhary
    Signed-off-by: Suyash <suyash0405@gmail.com>

--- file01.js
+++ file01.js
@@ -3,6 +3,8 @@ function greet(name){
 console.log('hello ' + name)
 console.log('hello ' + name)
 console.log('hello ' + name)
+console.log("ipec gzb")
+console.log("chaudhary")
 
 }
 greet ("suyash")
